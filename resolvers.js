b82a650a5bbbcbae52a09d13b84c5dfb7ba439e1
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-17 14:46:12 +1100

    Remove test query

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,6 @@
 const db = require('./db');
 
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!',
     students: () => db.students.list(),
     student: (parent, { id }) => db.students.get(id),
     colleges: () => db.colleges.list(),
