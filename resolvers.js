77f1c8319377328bb135411c5c6dfd81b9a67a59
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-17 15:37:27 +1100

    Need to return college for valid input

--- resolvers.js
+++ resolvers.js
@@ -5,5 +5,31 @@ const Query = {
     student: (parent, { id }) => db.students.get(id),
     colleges: () => db.colleges.list(),
     college: (parent, { id }) => db.colleges.get(id),
+    collegeBy: (parent, { name, location, rating }) => {
+        if (name !== null && location !== null && rating !== null) {
+            return db.colleges.list().filter(clg => clg.name === name && clg.location === location && clg.rating === rating);
+        }
+        else if (name !== null && location === null && rating === null) {
+            return db.colleges.list().filter(clg => clg.name === name);
+        }
+        else if (name === null && location !== null && rating === null) {
+            return db.colleges.list().filter(clg => clg.location === location);
+        }
+        else if (name === null && location === null && rating !== null) {
+            return db.colleges.list().filter(clg => clg.rating === rating);
+        }
+        else if (name !== null && location !== null && rating === null) {
+            return db.colleges.list().filter(clg => clg.name === name && clg.location === location);
+        }
+        else if (name !== null && location === null && rating !== null) {
+            return db.colleges.list().filter(clg => clg.name === name && clg.rating === rating);
+        }
+        else if (name === null && location !== null && rating !== null) {
+            return db.colleges.list().filter(clg => clg.location === location && clg.rating === rating);
+        }
+        else {
+            return [];
+        }
+    },
  }
  module.exports = {Query}
