132d11a1143546fdd3e8c0ebdbba027d3e910bad
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-17 14:37:25 +1100

    Get student by id

--- resolvers.js
+++ resolvers.js
@@ -3,6 +3,7 @@ const db = require('./db');
 const Query = {
     test: () => 'Test Success, GraphQL server is up & running !!',
     students: () => db.students.list(),
+    student: (parent, { id }) => db.students.get(id),
     colleges: () => db.colleges.list(),
  }
  module.exports = {Query}
