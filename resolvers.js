c7db1cc8555d4e520e976711fe9872e3774e2664
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-17 14:52:11 +1100

    Get college by id

--- resolvers.js
+++ resolvers.js
@@ -4,5 +4,6 @@ const Query = {
     students: () => db.students.list(),
     student: (parent, { id }) => db.students.get(id),
     colleges: () => db.colleges.list(),
+    college: (parent, { id }) => db.colleges.get(id),
  }
  module.exports = {Query}
