1d19f002426b92056d1b50c43b47d6e009dff260
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-17 15:47:56 +1100

    CollegeParameters with values being received in resolver as undefined

--- resolvers.js
+++ resolvers.js
@@ -6,25 +6,28 @@ const Query = {
     colleges: () => db.colleges.list(),
     college: (parent, { id }) => db.colleges.get(id),
     collegeBy: (parent, { name, location, rating }) => {
-        if (name !== null && location !== null && rating !== null) {
+        console.log(name);
+        console.log(location);
+        console.log(rating);
+        if (name !== undefined && location !== undefined && rating !== undefined) {
             return db.colleges.list().filter(clg => clg.name === name && clg.location === location && clg.rating === rating);
         }
-        else if (name !== null && location === null && rating === null) {
+        else if (name !== undefined && location === undefined && rating === undefined) {
             return db.colleges.list().filter(clg => clg.name === name);
         }
-        else if (name === null && location !== null && rating === null) {
+        else if (name === undefined && location !== undefined && rating === undefined) {
             return db.colleges.list().filter(clg => clg.location === location);
         }
-        else if (name === null && location === null && rating !== null) {
+        else if (name === undefined && location === undefined && rating !== undefined) {
             return db.colleges.list().filter(clg => clg.rating === rating);
         }
-        else if (name !== null && location !== null && rating === null) {
+        else if (name !== undefined && location !== undefined && rating === undefined) {
             return db.colleges.list().filter(clg => clg.name === name && clg.location === location);
         }
-        else if (name !== null && location === null && rating !== null) {
+        else if (name !== undefined && location === undefined && rating !== undefined) {
             return db.colleges.list().filter(clg => clg.name === name && clg.rating === rating);
         }
-        else if (name === null && location !== null && rating !== null) {
+        else if (name === undefined && location !== undefined && rating !== undefined) {
             return db.colleges.list().filter(clg => clg.location === location && clg.rating === rating);
         }
         else {
