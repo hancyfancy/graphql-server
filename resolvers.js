bce146a58f6d73943ca3686bd5043b3b3e58933b
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-17 14:16:48 +1100

    Retrieve all colleges

--- resolvers.js
+++ resolvers.js
@@ -2,6 +2,7 @@ const db = require('./db');
 
 const Query = {
     test: () => 'Test Success, GraphQL server is up & running !!',
-    students: () => db.students.list()
+    students: () => db.students.list(),
+    colleges: () => db.colleges.list(),
  }
  module.exports = {Query}
