d6b0adf72724a4cb13e7ee1bd306d0d6a1a9127a
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-17 13:16:48 +1100

    Local graphql server

---
+++ db.js
@@ -0,0 +1,10 @@
+//https://www.tutorialspoint.com/graphql/graphql_environment_setup.htm
+
+const { DataStore } = require('notarealdb');
+
+const store = new DataStore('./data');
+
+module.exports = {
+   students:store.collection('students'),
+   colleges:store.collection('colleges')
+};
